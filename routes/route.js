4a7fd432dbcfee1a6110dc6a2e2dc9c3b8342e4f
abirdeveloper <abirindra@rocketmail.com>
2025-12-02 12:50:33 +0000

    Apply fixes suggested by AI reviewer

--- routes/route.js
+++ routes/route.js
@@ -2,47 +2,75 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controller')
 
-router.get('/', (req, res) => {  
+router.get('/', (req, res) => {
     res.send('Welcome to Book Store')
 })
 
-router.get('/books', async(req, res) => {
-    const data = await controller.getAll()
-    res.send(data)
-})
-
-router.get('/book/:code', async(req,res) => {
-    const { code } = req.params
-    const data = await controller.getOne(code)
-    res.send(data)
-})
-
-router.post('/addbook', (req, res) => {
-    const newBook = {
-        code: req.body.code,
-        name: req.body.name,
-        author:req.body.author
+router.get('/books', async (req, res) => {
+    try {
+        const data = await controller.getAll()
+        res.send(data)
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
-    
-    controller.addNewBook(newBook)
-    res.send('Book added!!')
 })
 
-router.post('/updatebook', (req, res) => {
-    const newBook = {
-        code: req.body.code,
-        name: req.body.name,
-        author:req.body.author
+router.get('/book/:code', async (req, res) => {
+    try {
+        const { code } = req.params
+        const data = await controller.getOne(code)
+        if (!data) {
+            return res.status(404).send('Book not found');
+        }
+        res.send(data)
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
-    
-    controller.updateNewBook(newBook)
-    res.send('1 book updated!!')
 })
 
-router.post('/deletebook',(req,res)=>{
-    let code = req.body.code
-    controller.deleteBookByCode(code)
-    res.send('Book Deleted!!')
+router.post('/addbook', async (req, res) => {
+    try {
+        const newBook = {
+            code: req.body.code,
+            name: req.body.name,
+            author: req.body.author
+        }
+
+        await controller.addNewBook(newBook)
+        res.send('Book added!!')
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
+router.post('/updatebook', async (req, res) => {
+    try {
+        const newBook = {
+            code: req.body.code,
+            name: req.body.name,
+            author: req.body.author
+        }
+
+        await controller.updateNewBook(newBook)
+        res.send('1 book updated!!')
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
+router.post('/deletebook', async (req, res) => {
+    try {
+        let code = req.body.code
+        await controller.deleteBookByCode(code)
+        res.send('Book Deleted!!')
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 })
 
 // router.get('/book/:code', async(req,res) => {
